6499ef7ca0e9991cfd45a3f3720ea9531e232c4f
fabiodigiacomopepe <fabiodgp20@gmail.com>
2023-11-23 11:35:32 +0100

    Add redirect on /photos

---
+++ spring-il-mio-fotoalbum/src/main/java/org/lessons/java/springilmiofotoalbum/controller/IndexController.java
@@ -0,0 +1,17 @@
+package org.lessons.java.springilmiofotoalbum.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+
+    // Faccio il redirect se l'utente atterra su "/"
+    @GetMapping
+    public String home() {
+        return "redirect:/photos";
+    }
+}
+
