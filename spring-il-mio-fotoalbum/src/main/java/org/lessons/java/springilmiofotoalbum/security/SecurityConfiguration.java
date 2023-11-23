6645de636bdd00c0c09c7f08b11c15369511a68a
fabiodigiacomopepe <fabiodgp20@gmail.com>
2023-11-23 15:26:13 +0100

    Added path in SecurityConfiguration to manage access

--- spring-il-mio-fotoalbum/src/main/java/org/lessons/java/springilmiofotoalbum/security/SecurityConfiguration.java
+++ spring-il-mio-fotoalbum/src/main/java/org/lessons/java/springilmiofotoalbum/security/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package org.lessons.java.springilmiofotoalbum.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -14,6 +15,12 @@ public class SecurityConfiguration {
     // Con questo metodo gestisco l'autorizzazione di accesso alle varie rotte
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests()
+                .requestMatchers("/photos", "/photos/show/**").hasAuthority("ADMIN")                // Index/Show   PHOTO
+                .requestMatchers("/users").hasAuthority("ADMIN")                                    // Index        USERS
+                .requestMatchers("/photos/create").hasAuthority("ADMIN")                            // Create       PHOTO
+                .requestMatchers("/photos/edit/**").hasAuthority("ADMIN")                           // Edit         PHOTO
+                .requestMatchers("/photos/delete/**").hasAuthority("ADMIN")                         // Delete       PHOTO
+                .requestMatchers(HttpMethod.POST, "/photos/**").hasAuthority("ADMIN")               // Metodi POST  PHOTO
                 .requestMatchers("/**").permitAll()
                 .and().formLogin()
                 .and().logout();
