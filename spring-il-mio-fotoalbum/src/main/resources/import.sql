56478d32d1b49469ea1a3236483ca49d615e5197
fabiodigiacomopepe <fabiodgp20@gmail.com>
2023-11-24 16:09:58 +0100

    Added super user in DB (import.sql)

--- spring-il-mio-fotoalbum/src/main/resources/import.sql
+++ spring-il-mio-fotoalbum/src/main/resources/import.sql
@@ -1,5 +1,6 @@
 INSERT INTO roles (id, name) VALUES(1, 'ADMIN');
 INSERT INTO roles (id, name) VALUES(2, 'USER');
+INSERT INTO roles (id, name) VALUES(3, 'SUPER_ADMIN');
 
 INSERT INTO users (first_name, last_name, email, password) VALUES('Mario', 'Rossi', 'mariorossi@gmail.com', '{noop}mario');
 INSERT INTO users (first_name, last_name, email, password) VALUES('Luca', 'Bianchi', 'lucabianchi@gmail.com', '{noop}luca');
@@ -7,6 +8,7 @@ INSERT INTO users (first_name, last_name, email, password) VALUES('Fabio', 'Neri
 
 INSERT INTO users_roles (user_id, roles_id) VALUES(1, 1);
 INSERT INTO users_roles (user_id, roles_id) VALUES(1, 2);
+INSERT INTO users_roles (user_id, roles_id) VALUES(1, 3);
 INSERT INTO users_roles (user_id, roles_id) VALUES(2, 1);
 INSERT INTO users_roles (user_id, roles_id) VALUES(2, 2);
 INSERT INTO users_roles (user_id, roles_id) VALUES(3, 2);
